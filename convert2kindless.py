24937bb16d05508849b8ed1343f8c2dcb833df03
Orion Chang <ism.zjx@gmail.com>
2017-02-01 15:09:42 +0800

    Adjusting several stuff
    Import from PIL
    Add support for kpw3
    Take care of naming

--- convert2kindless.py
+++ convert2kindless.py
@@ -1,7 +1,7 @@
-import Image, ImageOps
+from PIL import Image, ImageOps
 import re, os, sys, errno
 
-SIZE = 600,800
+SIZE = 1072,1448
 IMAGEEXT = re.compile(".*\.(jpg|jpeg)", re.IGNORECASE)
 
 def ensuredirs(path):
@@ -19,24 +19,31 @@ except:
     print "Please specify a directory"
     exit(1)
 
+
+counter = 0
+
 files = os.listdir(image_dir)
 for file in files:
     if IMAGEEXT.match(file):
         f = os.path.join(image_dir, file)
         print f
         img = Image.open(f)
-
+        
         # rotate landscape images
         if img.size[0] > img.size[1]:
             img = img.rotate(90)
-
-        # resize and crop the image to fit in 600x800 screen
+    
+        # resize and crop the image to fit in 1072*1448 screen
         img = ImageOps.fit(img, SIZE, Image.ANTIALIAS)
-
+        
         # convert to grayscale
         img = img.convert('L')
-
+        
+        # screensaver naming
+        file_name = "bg_ss" + str(counter).zfill(3) + ".png"
+        counter+=1
+        
         # save to ./ss directory
         ensuredirs(os.path.join(image_dir,'ss'))
-        fout = os.path.join(image_dir, 'ss', file)
-        img.save(fout, "JPEG")
+        fout = os.path.join(image_dir, 'ss', file_name)
+        img.save(fout, "PNG")
